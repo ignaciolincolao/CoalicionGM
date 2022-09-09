c385322cd2052135b2d2259bcb418e9745f18538
aparra <aparra2018@alu.uct.cl>
2022-09-09 17:57:39 -0300

    Añadido calculo de convex hull

--- CGM/src/main.cpp
+++ CGM/src/main.cpp
@@ -4,12 +4,11 @@
 #include <nlohmann/json.hpp> //json
 #include <string> //string
 #include <fstream> //ifstream
-//#include <boost/geometry.hpp>
-//#include <boost/geometry/algorithms/convex_hull.hpp> //convex hull
-
+#include <algorithm>
+#include <vector>
+#include <stack>
 using namespace std;
 using json = nlohmann::json;
-
 float minDist(float d[], bool genSet[], int n)
 {
     float min = FLT_MAX, min_index;
@@ -99,7 +98,76 @@ float dis_euc(float x1, float y1, float x2, float y2)
     float calculo = pow(pow((x2 - x1), 2) + pow((y2 - y1), 2), 1 / (float)2);
     return calculo;
 }
+struct Point
+{
+    float x, y;
+};
+float orientation(Point p, Point q, Point r)
+{
+    float val = (q.y - p.y) * (r.x - q.x) -
+        (q.x - p.x) * (r.y - q.y);
 
+    if (val == 0) return 0;  // collinear
+    return (val > 0) ? 1 : 2; // clock or counterclock wise
+}
+
+// Prints convex hull of a set of n points.
+void convexHull(Point points[], int n)
+{
+    // There must be at least 3 points
+    if (n < 3) return;
+
+    // Initialize Result
+    vector<Point> hull;
+
+    // Find the leftmost point
+    int l = 0;
+    for (int i = 1; i < n; i++)
+        if (points[i].x < points[l].x)
+            l = i;
+
+    // Start from leftmost point, keep moving counterclockwise
+    // until reach the start point again.  This loop runs O(h)
+    // times where h is number of points in result or output.
+    int p = l, q;
+    do
+    {
+        // Add current point to result
+        hull.push_back(points[p]);
+
+        // Search for a point 'q' such that orientation(p, q,
+        // x) is counterclockwise for all points 'x'. The idea
+        // is to keep track of last visited most counterclock-
+        // wise point in q. If any point 'i' is more counterclock-
+        // wise than q, then update q.
+        q = (p + 1) % n;
+        for (int i = 0; i < n; i++)
+        {
+            // If i is more counterclockwise than current q, then
+            // update q
+            if (orientation(points[p], points[i], points[q]) == 2)
+                q = i;
+        }
+
+        // Now q is the most counterclockwise with respect to p
+        // Set p as q for next iteration, so that q is added to
+        // result 'hull'
+        p = q;
+
+    } while (p != l);  // While we don't come to first point
+    // Print Result
+    cout << "X:" << endl;
+    for (int i = 0; i < hull.size(); i++)
+        cout << hull[i].x <<",";
+    cout << endl;
+    cout << "Y:" << endl;
+    for (int i = 0; i < hull.size(); i++)
+        cout << hull[i].y << ",";
+    cout << endl;
+}
+
+
+// ConvexHull obtenido de https://www.geeksforgeeks.org/convex-hull-set-1-jarviss-algorithm-or-wrapping/
 int main()
 {
     //cargar archivo de votaciones
@@ -207,6 +275,14 @@ int main()
         cout << CGM[i] << ",";
     }
     cout << endl << fitnessCGM << endl;
-    //calculo Convex hull
-    //boost::geometry::algorithms::convex_hull
+    // Cambiar 214 por tamaño Quorum
+    Point Pts[214];
+    for (size_t i = 0; i < quorum; i++)
+    {
+        //Pts.push_back(Point());
+        Pts[i].x = matPos[CGM[i]][0];
+        Pts[i].y = matPos[CGM[i]][1];
+    }
+    int size = sizeof(Pts) / sizeof(Pts[0]);
+    convexHull(Pts, size);
 }
