50b908061a79da4344a2bcea89335f0c3844ec54
Eduardo Lobos <eduardo.lobos03@gmail.com>
2022-09-20 21:22:25 -0300

    Termino de comentarios en código principal.

--- CGM/src/main.cpp
+++ CGM/src/main.cpp
@@ -1,57 +1,57 @@
-// coalition.cpp: define el punto de entrada de la aplicación.
-//
-#include <iostream> //general
-#include <nlohmann/json.hpp> //json
-#include <string> //string
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <string>
 #include <cstring>
-#include <fstream> //ifstream
+#include <fstream>
 #include <algorithm>
 #include <vector>
 #include <chrono>
 #include "functions.h"
+
 using namespace std;
 using json = nlohmann::json;
-double alpha = 0.99;//revisar
-double delta = 0.9; //0.95 es el limit
+
+// Default params
+double alpha = 0.99;
+double delta = 0.9; // 0.95 its a limit
 double delta2 = -1;
 string votes = "votes.json";
-int main(int argc, char* argv[])
+
+int main(int argc, char *argv[])
 {
+    // Params
     if (argc > 1)
     {
         alpha = stoi(argv[1]);
-        delta= stoi(argv[2]);
-        delta2= stoi(argv[3]);
+        delta = stoi(argv[2]);
+        delta2 = stoi(argv[3]);
         votes = stoi(argv[4]);
     }
-    //cargar file de votaciones
+    // Load voting file
     ifstream file(votes);
     json data = json::parse(file);
 
-    //se crea y abre el file de salida
+    // Create output file
     ofstream results;
     results.open("results.json");
 
-    //numero de parlamentario
+    // Number of congressmen
     int n = data["rollcalls"][0]["votes"].size();
-    //creacion de la matriz de distancia
-    double** distance_matrix = (double**)malloc(n * sizeof(double*));
+    // Creating the distance matrix
+    double **distance_matrix = (double **)malloc(n * sizeof(double *));
     for (size_t i = 0; i < n; i++)
     {
-        distance_matrix[i] = (double*)malloc(n * sizeof(double));
+        distance_matrix[i] = (double *)malloc(n * sizeof(double));
     }
-    //Creacion matriz de posiciones
-    double** position_matrix = (double**)malloc(n * sizeof(double*));
+    // Creation and filling of position matrix
+    double **position_matrix = (double **)malloc(n * sizeof(double *));
     for (size_t i = 0; i < n; i++)
     {
-        position_matrix[i] = (double*)malloc(2 * sizeof(double));
+        position_matrix[i] = (double *)malloc(2 * sizeof(double));
         position_matrix[i][0] = data["rollcalls"][0]["votes"][i]["x"];
-        //cout << fixed << position_matrix[i][0] << setprecision(9) << ",";
         position_matrix[i][1] = data["rollcalls"][0]["votes"][i]["y"];
-        //break;
     }
-    //rellenado de la matriz de distancia
-    // Matriz rellenando por completo
+    // Filling the distance matrix
     for (size_t i = 0; i < n; i++)
     {
         for (size_t j = 0; j < n; j++)
@@ -60,68 +60,59 @@ int main(int argc, char* argv[])
         }
     }
 
-    //inicializacion de quorum
+    // Quorum initialization
     int quorum = trunc(n / 2) + 1;
 
-    
-    //Calculo Tiempo Inicial
+    // Calculate start time
     auto initial_time = chrono::high_resolution_clock::now();
-    /////////////////////////////////////
-    ////// Ordenamiento algoritmo B
-    /////////////////////////////////////
-    int** congressmen = (int**)malloc(n * sizeof(int*));
+    // Congressmen matrix Initialization
+    int **congressmen = (int **)malloc(n * sizeof(int *));
     for (size_t i = 0; i < n; i++)
     {
-        congressmen[i] = (int*)malloc(quorum * sizeof(int));
+        congressmen[i] = (int *)malloc(quorum * sizeof(int));
     }
-
-    double* initial_fitness = (double*)malloc(n * sizeof(double));
-    int* initial_fitness_index = (int*)malloc(n * sizeof(int));
-
-    for (int j = 0; j < n; j++) {
+    // Fitness initialization
+    double *initial_fitness = (double *)malloc(n * sizeof(double));
+    int *initial_fitness_index = (int *)malloc(n * sizeof(int));
+    // Creation of initial solutions
+    for (int j = 0; j < n; j++)
+    {
         minDistEdge(congressmen[j], distance_matrix[j], n, quorum);
         sort_bubble(congressmen[j], quorum);
         initial_fitness[j] = evaluate_solution(congressmen[j], distance_matrix, quorum);
     }
-
-    /// Ordena los resultados
+    // Sort the solutions
     sort_bubble_index(initial_fitness_index, initial_fitness, n, n);
-    //sacar el mejor
-    int* coalition = (int*)malloc(quorum * sizeof(int));
+    // Create the variables for the best solution
+    int *coalition = (int *)malloc(quorum * sizeof(int));
     double fitness_minimum_winning_coalition;
-
-    memcpy(coalition, congressmen[initial_fitness_index[0]],sizeof(int)*quorum);
+    // bring out the best
+    memcpy(coalition, congressmen[initial_fitness_index[0]], sizeof(int) * quorum);
     fitness_minimum_winning_coalition = initial_fitness[initial_fitness_index[0]];
-    /////////////////////////////////////////////////////////////
-    /////// Fin poblacion inicial
-    ////////////////////////////////////////////////////////////
-    /*for (size_t i = 0; i < quorum; i++)
-    {
-        cout << coalition[i] << ",";
-    }
-    cout << endl << fitness << endl;*/
-    // Calculo Centroide de Coalicion
-    bool improve_posibility = true;
-    //inicio de punteros
-    double* centroid = (double*)malloc(2 * sizeof(double));
-    bool* grid_minimum_winning_coalition = (bool*)malloc(n * sizeof(bool));
-    int* not_in_minimum_winning_coalition = (int*)malloc((n - quorum) * sizeof(int));
-    int* possible_winning_coalition = (int*)malloc(quorum * sizeof(int));
-    //inicio vectores
-    struct Distance_vector* distance_vector_minimum_winning_coalition=(Distance_vector*)malloc(sizeof(struct Distance_vector)*(n-quorum));
+    // Pointer initialization
+    double *centroid = (double *)malloc(2 * sizeof(double));
+    bool *grid_minimum_winning_coalition = (bool *)malloc(n * sizeof(bool));
+    int *not_in_minimum_winning_coalition = (int *)malloc((n - quorum) * sizeof(int));
+    int *possible_winning_coalition = (int *)malloc(quorum * sizeof(int));
+    // Vector Initialization
+    struct Distance_vector *distance_vector_minimum_winning_coalition = (Distance_vector *)malloc(sizeof(struct Distance_vector) * (n - quorum));
     vector<Possible_improvement> improvement_vector;
-    //inicio variables
+    // Variable initialization
+    bool possibility_of_improvement = true;
     int counter;
     double sum = 0;
     double new_fitness;
     double fitness_copy;
-    //double* matDisHull;
-    // Cambiar 214 por tamaño Quorum
-    struct Point* Pts = (Point*)malloc(sizeof(struct Point) *quorum);
-    //Point Pts[quorum];
-    
+    double limit;
+    fitness_copy = fitness_minimum_winning_coalition;
+    int counter_posibility_of_improvement;
+    bool improvement = false;
+    int number_of_points = 0;
+    // Point Structure Initialization
+    struct Point *Pts = (Point *)malloc(sizeof(struct Point) * quorum);
+    // Calculate the centroid of the best solution
     calcularCentroide(centroid, position_matrix, coalition, quorum);
-
+    // save the points of the best solution
     for (size_t i = 0; i < quorum; i++)
     {
         Pts[i].x = position_matrix[coalition[i]][0];
@@ -129,9 +120,9 @@ int main(int argc, char* argv[])
         Pts[i].position = coalition[i];
         Pts[i].index = i;
     }
-    //size = sizeof(Pts) / sizeof(Pts[0]);
-    //auto hull = convexHull(Pts, size);
+    // Calculate the convex hull of the best solution
     auto hull = convexHull(Pts, quorum);
+    // Calculate the grid of the voting
     for (size_t i = 0; i < n; i++)
     {
         grid_minimum_winning_coalition[i] = 0;
@@ -140,51 +131,48 @@ int main(int argc, char* argv[])
     {
         grid_minimum_winning_coalition[coalition[i]] = 1;
     }
+    // Calculate number of points not in the best solution
     counter = 0;
     for (size_t i = 0; i < n; i++)
     {
-        if (!grid_minimum_winning_coalition[i]) {
+        if (!grid_minimum_winning_coalition[i])
+        {
             not_in_minimum_winning_coalition[counter] = i;
             counter++;
         }
     }
-    //matDisHull = nullptr;
-    //matDisHull = (double*)malloc(hull.size() * sizeof(double));
+    // Calculate the distance vector of the points not in the best solution towards the convex hull
     vector<DisHull> vector_distance_hull;
     vector_distance_hull = distance_of_hull_to_point(hull, centroid);
     distance_of_points_to_coalition(distance_vector_minimum_winning_coalition, not_in_minimum_winning_coalition, coalition, centroid, position_matrix, n, quorum);
-    /*
-    --1-Calcular cual de los elementos del convex hull estan mas lejos del centroid de la coalition
-    --2-Para cada punto que no forme la coalition calcular la sumatoria de las distancias a todos los puntos que si la forman
-    --3-Ordenar los que esten mas cerca a los que esten mas lejos
-    4-Tomar 1 punto que este mas cerca y probar intercambiando ese punto con el punto mas lejano del centroid y ver si improvement
-    5-Si no improvement con ninguno, tomar el segundo punto mas lejano del centroid y repetir proceso
-    */
-
-    //double limit = (vector_distance_hull[0].distance) * (alpha);
-
-    double limit;
-    fitness_copy = fitness_minimum_winning_coalition;
-    int counter_posibility_of_improvement;
-    bool improvement = false;
-    int number_of_points=0;
-    while (improve_posibility)
+    while (possibility_of_improvement)
     {
         delta2 = delta2 + 1;
+        // Calculate the limit
         limit = vector_distance_hull[0].distance * alpha * pow(delta, delta2);
-        for (size_t i = 0; i < (n-quorum); i++) if (distance_vector_minimum_winning_coalition[i].centroid_distance < limit) number_of_points++;
+        // Calculate the number of points that are within the limit
+        for (size_t i = 0; i < (n - quorum); i++)
+            if (distance_vector_minimum_winning_coalition[i].centroid_distance < limit)
+                number_of_points++;
+        // Reset the improvement counter
         counter_posibility_of_improvement = 0;
+
         for (size_t i = 0; i < hull.size(); i++)
         {
+            // Reset the improvement vector
             improvement_vector.clear();
             for (size_t j = 0; j < (number_of_points); j++)
             {
+                // Obtain the possible winning coalition
                 memcpy(possible_winning_coalition, coalition, sizeof(int) * quorum);
                 possible_winning_coalition[hull[vector_distance_hull[i].hull_index].index] = distance_vector_minimum_winning_coalition[j].position;
+                // sort the possible winning coalition
                 sort_bubble(possible_winning_coalition, quorum);
+                // Calculate the fitness of the possible winning coalition
                 new_fitness = evaluate_solution(possible_winning_coalition, distance_matrix, quorum);
                 if (new_fitness < fitness_minimum_winning_coalition)
                 {
+                    // prints
                     cout << endl;
                     cout << "Cantidad de puntos tomados:" << number_of_points << endl;
                     cout << "Fitness Anterior:" << fixed << fitness_minimum_winning_coalition << setprecision(9) << endl;
@@ -201,33 +189,45 @@ int main(int argc, char* argv[])
                         cout << possible_winning_coalition[i] << ",";
                     }
                     cout << endl;
+                    // Save the improvement
                     improvement_vector.push_back(Possible_improvement());
+                    // Save the fitness of the possible winning coalition
                     improvement_vector[counter_posibility_of_improvement].fitness = new_fitness;
+                    // Save the position of the possible winning coalition
                     improvement_vector[counter_posibility_of_improvement].index = j;
                     fitness_minimum_winning_coalition = new_fitness;
                     improvement = true;
                 }
             }
+            // sort the improvement vector
             sort(improvement_vector.begin(), improvement_vector.end(), &vecMejora_Sort);
             if (improvement)
             {
+                // A congressman from the convex hull is changed for one that is within the vector of possible improvement
                 coalition[hull[vector_distance_hull[i].hull_index].index] = distance_vector_minimum_winning_coalition[improvement_vector[0].index].position;
+                // sort the coalition
                 sort_bubble(coalition, quorum);
+                // Calculate the new fitness
                 fitness_minimum_winning_coalition = evaluate_solution(coalition, distance_matrix, quorum);
+                // We get out of the loop
                 improvement = false;
                 break;
             }
         }
+        // if the fitness does not change, we get out of the loop
         if (fitness_minimum_winning_coalition == fitness_copy)
         {
-            improve_posibility = false;
+            possibility_of_improvement = false;
         }
-        else {
+        else
+        {
+            // save the fitness
             fitness_copy = fitness_minimum_winning_coalition;
-            //nuevo convex hull
+            // Calculate the centroid
             calcularCentroide(centroid, position_matrix, coalition, quorum);
+            // Initialize the points again
             Pts = nullptr;
-            Pts = (Point*)malloc(sizeof(struct Point) * quorum);
+            Pts = (Point *)malloc(sizeof(struct Point) * quorum);
             for (size_t i = 0; i < quorum; i++)
             {
                 Pts[i].x = position_matrix[coalition[i]][0];
@@ -235,9 +235,9 @@ int main(int argc, char* argv[])
                 Pts[i].position = coalition[i];
                 Pts[i].index = i;
             }
-            //size = sizeof(Pts) / sizeof(Pts[0]);
-            //auto hull = convexHull(Pts, size);
+            // Calculate the convex hull
             auto hull = convexHull(Pts, quorum);
+            // Calculate the grid of the voting
             for (size_t i = 0; i < n; i++)
             {
                 grid_minimum_winning_coalition[i] = 0;
@@ -246,25 +246,29 @@ int main(int argc, char* argv[])
             {
                 grid_minimum_winning_coalition[coalition[i]] = 1;
             }
+            // Calculate number of points not in the best solution
             counter = 0;
             for (size_t i = 0; i < n; i++)
             {
-                if (!grid_minimum_winning_coalition[i]) {
+                if (!grid_minimum_winning_coalition[i])
+                {
                     not_in_minimum_winning_coalition[counter] = i;
                     counter++;
                 }
             }
+            // Clear the distance vector and calculate again
             vector_distance_hull.clear();
             vector_distance_hull = distance_of_hull_to_point(hull, centroid);
             distance_vector_minimum_winning_coalition = nullptr;
-            distance_vector_minimum_winning_coalition = (Distance_vector*)malloc(sizeof(struct Distance_vector) * (n - quorum));
+            distance_vector_minimum_winning_coalition = (Distance_vector *)malloc(sizeof(struct Distance_vector) * (n - quorum));
             distance_of_points_to_coalition(distance_vector_minimum_winning_coalition, not_in_minimum_winning_coalition, coalition, centroid, position_matrix, n, quorum);
         }
     }
+    // Calculate the time of the algorithm
     auto final_time = chrono::high_resolution_clock::now();
     double time_taken = chrono::duration_cast<chrono::nanoseconds>(final_time - initial_time).count();
     time_taken *= 1e-9;
-
+    // Print the results
     cout << "Algoritmo terminado - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
     cout << "Tiempo total:" << fixed << time_taken << setprecision(9) << endl;
     cout << "Fitness Final:" << fixed << fitness_minimum_winning_coalition << setprecision(9) << endl;
